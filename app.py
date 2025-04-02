9fd4cc08582377e1f80a735caba4da860f592c87
Bishal Bhusan sarma <107038308+BishalBhusanSarma@users.noreply.github.com>
2025-04-02 23:51:47 +0530

    app.py

--- app.py
+++ app.py
@@ -38,4 +38,5 @@ def remove_background():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  # Default to port 10000
+    app.run(host='0.0.0.0', port=port)
